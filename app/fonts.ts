71cf0aba8cb95db5bfbca52979f7e4ec2bed6d18
Samir <samiralammumtazalam@gmail.com>
2024-04-09 18:49:52 +0530

    fonts

---
+++ app/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter, Roboto_Mono, Comic_Neue } from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const comic_neue = Comic_Neue({
+    weight: '700',
+    subsets: ['latin']
+})
