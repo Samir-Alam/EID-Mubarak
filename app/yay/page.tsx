3c835b6d9ead9da609fd9d25340d37acddb27150
Samir <samiralammumtazalam@gmail.com>
2024-04-09 23:50:52 +0530

    updated yay

--- app/yay/page.tsx
+++ app/yay/page.tsx
@@ -1,19 +1,29 @@
+"use client"
+
 import Image from "next/image"
 import hug2 from "../../public/kitty/hug2.gif"
 import thank from "../../public/ty.gif"
 import cutie from "../../public/cutie.gif"
 import dance from "../../public/kitty/dance.gif"
+import { comic_neue } from "../fonts"
+import { useRouter } from "next/navigation"
 
 export default function yay () {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const router = useRouter();
+
     return (
-        <div className="flex flex-col gap-3 items-center">
+        <div className="flex flex-col gap-3 items-center mt-16">
             <div className="flex flex-row align-middle items-center justify-center">
                 <Image src={thank} alt="thank you" className="size-36"/>
                 <Image src={cutie} alt="cutie patootie" className="size-24 "/>
             </div>
-            <Image src={hug2} alt="My Cutie Pies"/>
-            <button className="bg-gray-500 rounded-lg w-28">
-                <span>Let&apos;s go back</span>
+            <Image src={hug2} alt="My Cutie Pies" className="size-1/2"/>
+            <button className="bg-gray-500 rounded-lg w-36 flex mt-48" onClick={() => router.push("/")}>
+                <span className={`${comic_neue.className} text-xl text-white`}>
+                    Let&apos;s go back
+                </span>
                 <Image src={dance} alt="Cute button" className="size-20"/>
             </button>
         </div>
