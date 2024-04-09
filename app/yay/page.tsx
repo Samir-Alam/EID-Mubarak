ac08d1c96b17b003df0267f92afd5e6b685a894e
Samir <samiralammumtazalam@gmail.com>
2024-04-09 23:20:39 +0530

    updated yay

--- app/yay/page.tsx
+++ app/yay/page.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image"
+import hug2 from "../../public/kitty/hug2.gif"
+import thank from "../../public/ty.gif"
+import cutie from "../../public/cutie.gif"
+
 export default function yay () {
     return (
-        <div>
+        <div className="flex flex-col gap-3 items-center">
+            <div className="flex flex-row align-middle items-center justify-center">
+                <Image src={thank} alt="thank you" className="size-36"/>
+                <Image src={cutie} alt="cutie patootie" className="size-24 "/>
+            </div>
+            <Image src={hug2} alt="My Cutie Pies"/>
             
         </div>
     )
